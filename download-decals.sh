c125ae78702b9f121840486e984406eaed7cf7ef
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2022-03-30 23:21:20 -0300

    do not download existing file

--- download-decals.sh
+++ download-decals.sh
@@ -7,15 +7,20 @@ pixscale=0.262
 bands=grz
 size=2048
 
-file="fits-cutout?ra=${ra}&dec=${dec}&layer=${layer}&pixscale=${pixscale}&bands=${bands}&size=${size}"
-url="https://www.legacysurvey.org/viewer/${file}"
+output=${cluster}__${bands}.fits
 
-path=previews/$cluster
-if [ ! -d $path ]
+# do not download if it already exists (must erase manually to force for now)
+if [ ! -f ${path}/$output ]
 then
-    mkdir $path
-fi
-wget "$url"
+    file="fits-cutout?ra=${ra}&dec=${dec}&layer=${layer}&pixscale=${pixscale}&bands=${bands}&size=${size}"
+    url="https://www.legacysurvey.org/viewer/${file}"
 
-newfile=${cluster}__${bands}.fits
-mv "$file" ${path}/$newfile
+    path=previews/$cluster
+    if [ ! -d $path ]
+    then
+        mkdir $path
+    fi
+    wget "$url"
+
+    mv "$file" ${path}/$output
+fi
