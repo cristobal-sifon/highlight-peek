9946aaee00a6a5b0265e049f1a8caa84b222c0bb
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2022-03-30 23:19:38 -0300

    better download directory tree

--- download-decals.sh
+++ download-decals.sh
@@ -9,11 +9,13 @@ size=2048
 
 file="fits-cutout?ra=${ra}&dec=${dec}&layer=${layer}&pixscale=${pixscale}&bands=${bands}&size=${size}"
 url="https://www.legacysurvey.org/viewer/${file}"
-if [ ! -d $cluster ]
+
+path=previews/$cluster
+if [ ! -d $path ]
 then
-    mkdir $cluster
+    mkdir $path
 fi
 wget "$url"
 
 newfile=${cluster}__${bands}.fits
-mv "$file" ${cluster}/$newfile
+mv "$file" ${path}/$newfile
